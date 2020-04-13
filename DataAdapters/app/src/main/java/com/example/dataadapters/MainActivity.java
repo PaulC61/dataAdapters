d47a53e0d0d0299fb8409336c2dfa02893b54870
PaulC61 <paulclarke61@gmail.com>
2020-04-13 14:14:37 +0200

    Array, ArrayAdapter and ListView created in MainActivity and linked to the ListView in activity_main.xml

--- DataAdapters/app/src/main/java/com/example/dataadapters/MainActivity.java
+++ DataAdapters/app/src/main/java/com/example/dataadapters/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dataadapters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] names = new String[10];
+        names [0] = "Paul";
+        names [1] = "Kanya";
+        names [2] = "Peter";
+        names [3] = "Nadine";
+        names [4] = "Lourenco";
+        names [5] = "Emiliano";
+        names [6] = "Rodrigo";
+        names [7] = "Danilo";
+        names [8] = "Rainer";
+        names [9] = "Felipe";
+
+        ArrayAdapter<String> studentNamesAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+        ListView studentListView = (ListView) findViewById(R.id.studentList);
+        studentListView.setAdapter(studentNamesAdapter);
+
     }
 }
